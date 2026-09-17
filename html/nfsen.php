6945bb23ac999ccecbb33549bda696e652352501
Peter Haag <peter@people.ops-trust.net>
2026-09-17 14:20:56 +0200

    Notes for nfdump-1.8.x and bump to 1.3.12

--- html/nfsen.php
+++ html/nfsen.php
@@ -33,7 +33,7 @@
 // The very first function to call
 session_start();
 
-$expected_version = "1.3.11";
+$expected_version = "1.3.12";
 
 // Session check
 if ( array_key_exists('backend_version', $_SESSION ) && $_SESSION['backend_version'] !=  $expected_version ) {
